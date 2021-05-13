e6ad17e019dfe14f28989d17c9a4ff5fd9017d8c
uriel181004 <82549982+uriel181004@users.noreply.github.com>
2021-05-12 21:59:00 -0500

    Add files via upload

---
+++ Ex 2 .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+    /*Amador Salinas Manuel Uriel
+    4iV17*/
+
+    float a, b, c;
+    printf("\n\n Programa 2");
+    printf("\n\n Suma de numeros enteros");
+    printf("\n\n Dame el valor entero de a:");
+    scanf("%f", &a);
+    printf("\n\n Dame el valor entero de b:");
+    scanf("%f", &b);
+    c= a + b;
+    printf("\n El producto es %d", c);
+    return 0;
+}
