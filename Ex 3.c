09279564bedcb98e4f52c3742610e7b1433edef3
uriel181004 <82549982+uriel181004@users.noreply.github.com>
2021-05-12 21:59:28 -0500

    Add files via upload

---
+++ Ex 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    /* Amador Slinas Manuel Uriel
+     12 MAY 21
+    */
+    float Fresa, kg, 13, precio;
+    printf ("\n\n comprando fruta");
+    printf ("\n\n Precio de cualquier fruta $13");
+    printf ("\n\n ingrese la fruta que selecciono");
+    scanf ("%f",&fresa);
+    printf ("\n\n ingrese el peso de la fruta seleccionada");
+    scanf ("%f",&kg);
+    precio= &kg * &13;
+    printf ("\n\n el precio total es %f",&precio);
+    return 0 ;
+} 
